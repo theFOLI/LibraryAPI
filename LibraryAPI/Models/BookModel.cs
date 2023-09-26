5c9b11d1fa3311ec5707e29e8ffc9023cc318035
theFOLI <f0l11l0f3654@gmail.com>
2023-09-26 08:13:51 -0300

    m

--- LibraryAPI/Models/BookModel.cs
+++ LibraryAPI/Models/BookModel.cs
@@ -1,10 +1,14 @@
 ﻿namespace LibraryAPI.Models
 {
-    public class SimpleBookData
+    public class BookModel
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string CoverImagePath { get; set; }
+        public string Sinopse { get; set; }
+        public int PageNumber { get; set; }
+        public int Year { get; set; }
+        public string Editora { get; set; }
     }
 }
